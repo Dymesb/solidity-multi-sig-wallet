5fa5bab4724754f860af7f9ba80a72861a03728a
Dymesb <72476691+Dymesb@users.noreply.github.com>
2020-10-25 18:00:14 -0300

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -59,7 +59,7 @@ module.exports = {
 
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    ropsten: {
+    live: {
       // provider: () =>
       //   new HDWalletProvider(
       //     mnemonic,
